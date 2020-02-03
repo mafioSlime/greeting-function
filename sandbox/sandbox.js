94771ffd8cb74b13532994d1ff9bbf0697832138
John Paguirigan <johnjaroldxvi@gmail.com>
2020-02-03 08:33:49 -0700

    javascript functionalities and dom manipulation

---
+++ sandbox/sandbox.js
@@ -0,0 +1,19 @@
+const welcome = document.getElementById('welcome')
+
+
+const greetings = () => {
+    alert('Hello Stranger!')
+    let askName = prompt('What is your name?')
+    let name = askName
+
+    if (name === null) {
+        alert('Nice to meet you!')
+    } else {
+        alert(`Nice to meet you ${name}`)
+    }
+
+    welcome.innerHTML = `<h1>Welcome to our hub ${name}</h1>`
+}
+
+greetings()
+
