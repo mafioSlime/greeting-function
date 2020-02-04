f28d93b5bf8a71be3057edf9aa487506f696a76a
John Paguirigan <johnjaroldxvi@gmail.com>
2020-02-04 08:57:39 -0700

    array methods converted into OOP

--- arrays/array.js
+++ arrays/array.js
@@ -1,27 +1,53 @@
 // EXERCISE #1
 
-const fruits = ['apple', 'banana', 'peach']
+// const fruits = ['apple', 'banana', 'peach']
 
-const addToList = (category, item) => {
-    category.push(item)
+// const addToList = (category, item) => {
+//     category.push(item)
+// }
+
+// const removeLastItem = (category) => {
+//     category.pop()
+// }
+
+// const addItemFirst = (category, item) => {
+//     category.unshift(item)
+// }
+
+// const removeFirstItem = (category) => {
+//     category.shift()
+// }
+
+
+// addToList(fruits, 'mango')
+// console.log(fruits)
+// addItemFirst(fruits, 'strawberry')
+// console.log(fruits)
+// removeFirstItem(fruits)
+// console.log(fruits)
+
+// OOP 
+
+class category {
+    constructor(item, price) {
+        this.item = item
+        this.price = `$${price}`
+    }
+    renameItem(newItem) {
+        this.item = newItem
+        return this
+  
+    }
+    changePrice(newPrice) {
+        this.price = `$${newPrice}`
+        return this
+    }
 }
 
-const removeLastItem = (category) => {
-    category.pop()
-}
+const fruit1 = new category('Banana', 2)
+const fruit2 = new category('Strawberry', 3.75)
 
-const addItemFirst = (category, item) => {
-    category.unshift(item)
-}
+fruit1.renameItem('Kiwi').changePrice(4.78)
 
-const removeFirstItem = (category) => {
-    category.shift()
-}
+console.log(fruit1)
 
-
-addToList(fruits, 'mango')
-console.log(fruits)
-addItemFirst(fruits, 'strawberry')
-console.log(fruits)
-removeFirstItem(fruits)
-console.log(fruits)
