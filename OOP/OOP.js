564546b347041c8a8cccc7a8368abf28993df5bb
John Paguirigan <johnjaroldxvi@gmail.com>
2020-02-03 23:09:58 -0700

    added method

--- OOP/OOP.js
+++ OOP/OOP.js
@@ -1,25 +1,35 @@
 class User {
     constructor(username, email){
-        this.username = username;
+        this.username = username
         this.email = email
+        this.score = 0
+    }
+    login() {
+        console.log(`${this.username} has logged in`)
+        return this
+    }
+    logout() {
+        console.log(`${this.username} has logged out`)
+        return this
+    }
+    incScore() {
+        this.score ++
+        return this
+    }
+}
+
+class Admin extends User {
+    deleteUser(user) {
+        users = users.filter(u => u.username !== user.username)
     }
 }
 
 const userOne = new User('mafiosoSlime', 'mafiosoSlime@gmail.com')
 const userTwo = new User('kingOfSlime', 'kingOfSlime@gmail.com')
+const userThree = new Admin('slimeJXVI', 'johnjaroldxvi@gmail.com')
 
-console.log(userOne, userTwo)
+users = [userOne, userTwo, userThree]
 
-// INSTEAD OF DOING THIS
+userThree.deleteUser(userTwo)
+console.log(users)
 
-// const userOne = { 
-//     userName: 'mafiosoSlime',
-//     email: 'mafiosoSlime@gmail.com'
-// }
-
-// const userTwo = {
-//     userName: 'kingOfSlime',
-//     email: 'kingOfSlime@gmail.com'
-// }
-
-// console.log(userOne, userTwo)
